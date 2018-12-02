e54b3ed2daf3e813ba545cdf5e6ff5a18b306374
Gregory <gregory.ferguson2@mavs.uta.edu>
2018-12-02 15:51:29 -0600

    Added Game Over Window

---
+++ SpaceGame-master/driver.cpp
@@ -0,0 +1,35 @@
+#include "mainheader.h"
+#include <gtkmm.h>
+#include <iostream>
+
+int main(int argc, char * argv[]) {
+
+    Gtk::Main app(argc, argv);
+    MainWindow window;
+    Gtk::Main::run(window);
+
+    if (check == 1) {
+        ShipWindow window2;
+        Gtk::Main::run(window2);
+    }
+
+    while (true) {
+        if (ship_type > 0) {
+            PlayWindow window4;
+            Gtk::Main::run(window4);
+
+            CombatWindow window5;
+            Gtk::Main::run(window5);
+
+            if(over)
+            {
+              GameOverWindow window8;
+              Gtk::Main::run(window8);
+
+            }
+
+        }
+    }
+
+    return 0;
+}
